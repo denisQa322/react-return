19d1724a2edfe7e74d1ed5a52dac66d573c6eaef
denisQa322 <denis.isakov.99@gmail.com>
2025-02-11 00:04:08 +0500

    small fixes

--- src/pages/Reject.tsx
+++ src/pages/Reject.tsx
@@ -1,28 +1,12 @@
-import { useState, useEffect } from "react";
-import useReturnFilters from "../hooks/useReturnFilters";
-import useLocalStorage from "../hooks/useLocalStorage";
-import ReturnList from "../components/ReturnList/ReturnList";
+import { useEffect } from "react";
+
 import LoadingIndicator from "../components/LoadingIndicator/LoadingComponent";
-import { ReturnItemProps } from "../types/returns";
 
 const Reject: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const { state: returns, update: setReturns } = useLocalStorage<
-    ReturnItemProps[]
-  >("returns", []);
-
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      const savedReturns = JSON.parse(localStorage.getItem("returns") || "[]");
-      setReturns(savedReturns);
-      console.log(setReturns(savedReturns));
-      setLoading(false);
-    }, 1000);
+    setTimeout(() => {}, 1000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const filterProps = useReturnFilters(returns);
 
   return (
     <main className="container">
